ab34b3c2aa5190592b7f132b30089054e3ba9039
Robert Dorna <rdorna8@gmail.com>
2023-12-29 16:01:36 +0100

    feat: implement Navigation component (closes #30)

---
+++ src/components/molecules/navigation.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Logo from "../atoms/logo";
+import Button from "../atoms/button";
+
+export default function Navigation() {
+  return (
+    <nav
+      className="
+      flex flex-row items-center justify-between w-full pr-md
+      border-b-[1px] border-color-silenced
+      sticky top-0
+      bg-color-primary
+    "
+    >
+      <a className="px-md" href="/">
+        <Logo small />
+      </a>
+      <Button text="Online Account" target="account" border={false} />
+    </nav>
+  );
+}
