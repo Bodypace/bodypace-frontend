a59fbd4b58ad2f2bcbaad5912db861ee9330201a
Robert Dorna <rdorna8@gmail.com>
2023-12-29 16:26:10 +0100

    feat: implement Footer component (closes #29)

---
+++ src/components/molecules/footer.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Link from "../atoms/link";
+
+export default function Footer() {
+  return (
+    <footer
+      className="
+      flex flex-row justify-center items-center
+      w-full gap-xl py-xl
+      border-t-[1px] border-color-silenced border-dashed
+    "
+    >
+      <Link text="Terms and Conditions" small target="terms-and-conditions" />
+      <Link text="Privacy Policy" small target="privacy-policy" />
+      <Link text="About Us" small target="about-us" />
+    </footer>
+  );
+}
---
+++ stories/components/molecules/footer.stories.ts
@@ -0,0 +1,42 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { within, expect } from "@storybook/test";
+
+import Footer from "@/components/molecules/footer";
+
+const meta = {
+  title: "Molecules/Footer",
+  component: Footer,
+  parameters: {
+    layout: "fullscreen",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const footer = canvas.getByRole("contentinfo");
+    await expect(footer).toBeInTheDocument();
+
+    const links = canvas.getAllByRole("link");
+    await expect(links).toHaveLength(3);
+
+    const termsLink = links[0];
+    await expect(termsLink).toBeInTheDocument();
+    await expect(termsLink).toHaveTextContent("Terms and Conditions");
+    await expect(termsLink).toHaveAttribute("href", "terms-and-conditions");
+
+    const privacyLink = links[1];
+    await expect(privacyLink).toBeInTheDocument();
+    await expect(privacyLink).toHaveTextContent("Privacy Policy");
+    await expect(privacyLink).toHaveAttribute("href", "privacy-policy");
+
+    const aboutUsLink = links[2];
+    await expect(aboutUsLink).toBeInTheDocument();
+    await expect(aboutUsLink).toHaveTextContent("About Us");
+    await expect(aboutUsLink).toHaveAttribute("href", "about-us");
+  },
+};
