518bf84af1d64e0fc4ffd6d6b34a7bd4ab9b07f2
Robert Dorna <rdorna8@gmail.com>
2024-02-20 18:15:50 +0100

    test: refactor Button stories - use one meta.play() instead of many calls to helper function

--- stories/components/atoms/button.stories.ts
+++ stories/components/atoms/button.stories.ts
@@ -3,6 +3,8 @@ import { within, expect, fn, userEvent } from "@storybook/test";
 
 import Button from "@/components/atoms/button";
 
+// TODO: test hover state
+
 const meta = {
   title: "Atoms/Button",
   component: Button,
@@ -13,80 +15,82 @@ const meta = {
   argTypes: {
     wide: { control: "boolean" },
   },
+  play: async ({ canvasElement, args }) => {
+    const variant = typeof args.onClick === "string" ? "link" : "button";
+    const mode = !!args.onClick ? "enabled" : "disabled";
+    const role = variant === "link" && mode === "disabled" ? null : variant;
+
+    const user = userEvent.setup();
+    const canvas = within(canvasElement);
+    const element = role
+      ? canvas.getByRole(role, { name: "Click me" })
+      : canvas.getByText("Click me");
+
+    await expect(element).toBeInTheDocument();
+
+    if (role === "link") {
+      await expect(element).toHaveAttribute("href", "some_url");
+    }
+
+    if (role === null) {
+      await expect(element.getAttribute("role")).toBe(null);
+    }
+
+    if (role === "button") {
+      if (mode === "enabled") {
+        await expect(element).not.toHaveAttribute("disabled");
+        await expect(args.onClick).toHaveBeenCalledTimes(0);
+        await user.click(element);
+        await expect(args.onClick).toHaveBeenCalledTimes(1);
+      } else {
+        await expect(element).toHaveAttribute("disabled");
+      }
+    }
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Enabled: Story = defineStory("button", "enabled", {
-  text: "Click me",
-  wide: false,
-  onClick: fn(),
-});
+export const Enabled: Story = {
+  args: {
+    text: "Click me",
+    wide: false,
+    onClick: fn(),
+  },
+};
 
-export const Disabled: Story = defineStory("button", "disabled", {
-  ...Enabled.args,
-  onClick: undefined,
-});
+export const Disabled: Story = {
+  args: {
+    ...Enabled.args,
+    onClick: undefined,
+  },
+};
 
-export const EnabledLink: Story = defineStory("link", "enabled", {
-  ...Enabled.args,
-  onClick: "some_url",
-});
+export const EnabledLink: Story = {
+  args: {
+    ...Enabled.args,
+    onClick: "some_url",
+  },
+};
 
-export const DisabledLink: Story = defineStory("link", "disabled", {
-  ...Enabled.args,
-  onClick: "",
-});
+export const DisabledLink: Story = {
+  args: {
+    ...Enabled.args,
+    onClick: "",
+  },
+};
 
-export const EnabledWide: Story = defineStory("button", "enabled", {
-  ...Enabled.args,
-  wide: true,
-});
+export const EnabledWide: Story = {
+  args: {
+    ...Enabled.args,
+    wide: true,
+  },
+};
 
-export const DisabledWide: Story = defineStory("button", "disabled", {
-  ...EnabledWide.args,
-  onClick: undefined,
-});
-
-function defineStory(
-  variant: "button" | "link",
-  mode: "enabled" | "disabled",
-  spec: Story["args"],
-): Story {
-  return {
-    args: {
-      ...spec,
-    },
-    play: async ({ canvasElement, args }) => {
-      const role = variant === "link" && mode === "disabled" ? null : variant;
-
-      const user = userEvent.setup();
-      const canvas = within(canvasElement);
-      const element = role
-        ? canvas.getByRole(role, { name: "Click me" })
-        : canvas.getByText("Click me");
-
-      await expect(element).toBeInTheDocument();
-
-      if (role === "link") {
-        await expect(element).toHaveAttribute("href", "some_url");
-      }
-
-      if (role === null) {
-        await expect(element.getAttribute("role")).toBe(null);
-      }
-
-      if (role === "button") {
-        if (mode === "enabled") {
-          await expect(element).not.toHaveAttribute("disabled");
-          await expect(args.onClick).toHaveBeenCalledTimes(0);
-          await user.click(element);
-          await expect(args.onClick).toHaveBeenCalledTimes(1);
-        } else {
-          await expect(element).toHaveAttribute("disabled");
-        }
-      }
-    },
-  };
-}
+export const DisabledWide: Story = {
+  args: {
+    ...EnabledWide.args,
+    onClick: undefined,
+  },
+};
