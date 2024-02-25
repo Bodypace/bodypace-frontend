d4f4010e02dc02207b514a0ae2837b84547a759b
Robert Dorna <rdorna8@gmail.com>
2024-02-25 14:01:02 +0100

    feat: implement File component (closes #34)

---
+++ src/components/molecules/file.tsx
@@ -0,0 +1,50 @@
+import Checkbox, { type CheckboxProps } from "../atoms/checkbox";
+
+export interface FileProps {
+  encrypted: boolean;
+  no: number;
+  filename: string;
+  size: string;
+  checked: CheckboxProps["checked"];
+  onChange: CheckboxProps["onChange"];
+}
+
+export function File({
+  encrypted,
+  no,
+  filename,
+  size,
+  checked,
+  onChange,
+}: FileProps) {
+  return (
+    <div
+      className="
+        flex items-center w-[700px] h-[69px] py-sm gap-md border-b-[1px] border-color-accent
+      "
+    >
+      <span className="font-technical text-md text-color-primary">{no}.</span>
+      <Checkbox
+        labelledBy={`file-no-${no}`}
+        checked={checked}
+        onChange={onChange}
+        small
+      />
+      <label
+        id={`file-no-${no}`}
+        data-encrypted={!!encrypted}
+        className="
+          text-lg text-color-primary
+          data-[encrypted=true]:font-ciphertext
+          data-[encrypted=false]:font-plaintext
+        "
+      >
+        {filename}
+      </label>
+      <div className="flex flex-1" />
+      <span className="w-[100px] font-technical text-md text-color-primary">
+        {size}
+      </span>
+    </div>
+  );
+}
---
+++ stories/components/molecules/file.stories.tsx
@@ -0,0 +1,67 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { fn, within, expect, userEvent } from "@storybook/test";
+
+import { File } from "@/components/molecules/file";
+
+const meta = {
+  title: "Molecules/File",
+  component: File,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  play: async ({ args, canvasElement }) => {
+    const user = userEvent.setup();
+
+    const canvas = within(canvasElement);
+    const checkbox = canvas.getByRole("checkbox", {
+      // TODO: this label name is probably not ideal
+      name: args.filename,
+    });
+    await expect(checkbox).toBeInTheDocument();
+    await expect(checkbox).toHaveAttribute(
+      "aria-checked",
+      args.checked ? "true" : "false",
+    );
+
+    await expect(args.onChange).toHaveBeenCalledTimes(0);
+    await user.click(checkbox);
+    await expect(args.onChange).toHaveBeenCalledTimes(1);
+    await expect(args.onChange).toHaveBeenNthCalledWith(1, !args.checked);
+  },
+} satisfies Meta<typeof File>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DecryptedUnchecked: Story = {
+  args: {
+    encrypted: false,
+    no: 1,
+    filename: "blood_work.pdf",
+    size: "2.4MB",
+    checked: false,
+    onChange: fn(),
+  },
+};
+
+export const DecryptedChecked: Story = {
+  args: {
+    ...DecryptedUnchecked.args,
+    checked: true,
+  },
+};
+
+export const EncryptedUnchecked: Story = {
+  args: {
+    ...DecryptedUnchecked.args,
+    encrypted: true,
+  },
+};
+
+export const EncryptedChecked: Story = {
+  args: {
+    ...EncryptedUnchecked.args,
+    checked: true,
+  },
+};
