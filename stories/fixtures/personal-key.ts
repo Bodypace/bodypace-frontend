030f94c5b23d253135565e7ce3aa84656d1ef4cd
Robert Dorna <rdorna8@gmail.com>
2024-03-06 19:51:25 +0100

    test: assert that `mockedKey` fixture has valid values

--- stories/fixtures/personal-key.ts
+++ stories/fixtures/personal-key.ts
@@ -3,4 +3,12 @@ const mockedKey = {
   base64: "TW9ja2VkIHBlcnNvbmFsIGtleSBkb24ndCB1c2UgaXQ",
 };
 
+if (mockedKey.plain.length !== 32) {
+  throw new Error("Mocked personal key should be 32 bytes long");
+}
+
+if (!btoa(mockedKey.plain).startsWith(mockedKey.base64)) {
+  throw new Error("Mocked personal key should be base64 encoded correctly");
+}
+
 export default mockedKey;
