e0ed09681f55cc3b130dd6cc3c2b3ff1e1ef5964
Robert Dorna <rdorna8@gmail.com>
2024-03-07 22:48:11 +0100

    feat: implement Account page (closes #113)
    Name is probably a little bit misleading. It's the main dashboard page
    of a logged in user where user sees all their files.

---
+++ src/app/(pages)/account/(authenticated)/(dashboard)/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React from "react";
+import Button from "@/components/atoms/button";
+import { useFiles, type File } from "@/lib/files";
+import { Files } from "@/components/organisms/files";
+
+export default function AccountPage() {
+  const { files } = useFiles();
+  const [selection, setSelection] = React.useState<Set<File["id"]>>(new Set());
+  const selectedFilesCount = selection.size;
+
+  function handleSelect(id: File["id"], selected: boolean) {
+    if (selected) {
+      selection.add(id);
+      setSelection(new Set(selection));
+    } else {
+      selection.delete(id);
+      setSelection(new Set(selection));
+    }
+  }
+
+  function handleClearSelection() {
+    setSelection(new Set());
+  }
+
+  function handleUploadNewFile() {}
+
+  function handleDownloadFile() {}
+
+  function handleDeleteFile() {}
+
+  return (
+    <div className="flex flex-1 justify-center">
+      <div className="flex flex-col items-center gap-xl px-2xl pt-xl">
+        <span className="font-brand text-xl text-color-primary">
+          Your files
+        </span>
+        <div className="flex flex-col items-center gap-lg">
+          <Button text="Upload new file" wide onClick={handleUploadNewFile} />
+          <Button text="Settings" wide onClick="/account/settings" />
+        </div>
+        {files?.length ? (
+          <>
+            <span className="font-technical text-lg text-color-primary">
+              Selected {selectedFilesCount}{" "}
+              {selectedFilesCount === 1 ? "file" : "files"}
+            </span>
+            <div className="flex flex-col items-center gap-lg">
+              <Button
+                text="Download"
+                wide
+                onClick={selectedFilesCount ? handleDownloadFile : undefined}
+              />
+              <Button
+                text="Remove"
+                wide
+                onClick={selectedFilesCount ? handleDeleteFile : undefined}
+              />
+            </div>
+          </>
+        ) : undefined}
+      </div>
+      <div className="flex pt-lg">
+        <Files
+          selected={selection}
+          onSelected={handleSelect}
+          onClearSelection={handleClearSelection}
+        />
+      </div>
+    </div>
+  );
+}
--- src/app/(pages)/account/(authenticated)/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function AccountPage() {
-  return (
-    <div className="flex flex-1 text-xl font-light pt-2xl">
-      Not implemented yet
-    </div>
-  );
-}
