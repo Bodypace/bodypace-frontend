d7d17ba4ee732ab40726ad0f7e351fbf4c7f57e2
Robert Dorna <rdorna8@gmail.com>
2024-03-09 15:20:19 +0100

    feat: implement files deletion (closes #107)

--- stories/utils/mocking-files.tsx
+++ stories/utils/mocking-files.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { FilesContext, ProvideFiles } from "@/lib/files";
+import { FilesContext, ProvideFiles, type File, type Files } from "@/lib/files";
 import logger from "@/lib/logging";
+import { fn } from "@storybook/test";
+import { action } from "@storybook/addon-actions";
 
 export function MockFilesContext(Story: any, ctx: any) {
   const mock = ctx.parameters.filesContext;
@@ -15,9 +17,20 @@ export function MockFilesContext(Story: any, ctx: any) {
 export function SpyFilesContext(Story: any, ctx: any) {
   const files = ProvideFiles();
   logger.debug("mocking-files: SpyFilesContext: ", { files });
-  const spy: typeof files = {
+
+  const spy = React.useRef<Files>({
     files: files.files,
-  };
-  ctx.parameters.filesContext = spy;
+    deleteFiles: fn(),
+  });
+
+  spy.current.files = files.files;
+  (spy.current.deleteFiles as any).mockImplementation(
+    async (ids: File["id"][]) => {
+      action("deleteFiles")(ids);
+      return await files.deleteFiles(ids);
+    },
+  );
+
+  ctx.parameters.filesContext = spy.current;
   return MockFilesContext(Story, ctx);
 }
